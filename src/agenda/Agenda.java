095a1d58c23888bfcff2531883e5f4901ba4e44c
Tavo-01 <gusigus2002@gmail.com>
2022-06-02 17:50:03 +0200

    COMIT JAVADOC

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -15,7 +15,12 @@ public class Agenda {
     private HashMap<String, Contacto> contactos = new HashMap();
     private static int n = 0;
     private static int count = 0;
- 
+ /** 
+  * Añade un objeto contacto a la agenda
+  * @param contacto , objeto contacto que se quiere añadir
+  * @return devuelve contacto añadido si se realiza correctamente o exception
+  * @throws ContactoRepetidoException , excepcion especifica programada en el paquete exception
+  */
     public Contacto addContacto(Contacto contacto) throws ContactoRepetidoException {
         if (contactos.containsKey(contacto.getEmail())) {
             //
